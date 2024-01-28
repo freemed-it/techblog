cade61b83700ebc7a0de4978cfca5eab3f80a317
Seunghee Lee <49032882+eeseung@users.noreply.github.com>
2024-01-28 17:45:40 +0900

    feat: 공통 스타일 설정 (#5)
    * feat: Pretendard & NanumSquare Neo 폰트 추가
    
    * feat: tailwind.config freemed-red 컬러 추가
    
    * feat: globals.css 텍스트 스타일 정의
    
    * feat: RootLayout & 메인 페이지 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'freemed-red': {
+          light: '#fbd7d3',
+          DEFAULT: '#ed3827',
+        },
+      },
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'],
+        neo: ['NanumSquareNeoVariable', 'sans-serif'],
       },
     },
   },

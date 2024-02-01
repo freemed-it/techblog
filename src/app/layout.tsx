0db637d1d00ec4397ede386efe9290029824a3be
eeseung <eeseungh@gmail.com>
2024-02-01 18:13:42 +0900

    feat: GNB 컴포넌트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import GNB from '@/components/GNB'
 import type { Metadata } from 'next'
 import './globals.css'
 
@@ -9,7 +10,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <GNB />
+        <main className="pt-16">{children}</main>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 export default function Main() {
   return (
-    <main className="flex flex-col">
-      <div className="text-center">
-        <h1 className="heading hover:text-freemed-red">프리메드 기술 블로그</h1>
-        <h2 className="font-neo font-medium">프리메드 기술 블로그 Tech</h2>
-        <p className="body1">안녕하세요.</p>
-      </div>
-    </main>
+    <div className="flex flex-col text-center">
+      <h1 className="heading py-60 hover:text-freemed-red">프리메드 기술 블로그</h1>
+      <h2 className="bg-red-50 py-60 font-neo font-medium">프리메드 기술 블로그 Tech 개발</h2>
+      <p className="body1 bg-red-100 py-60">안녕하세요.</p>
+    </div>
   )
 }

0d5192a5ab713505e27afc37d899300fd25787e2
eeseung <eeseungh@gmail.com>
2024-03-02 17:28:45 +0900

    feat: RootLayout 정적 메타데이터 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,21 @@ import './globals.css'
 export const metadata: Metadata = {
   title: '프리메드 기술 블로그',
   description: '청년보건의료NGO 사단법인 프리메드 기술 블로그입니다.',
+  generator: 'Next.js',
+  applicationName: '프리메드 기술 블로그',
+  referrer: 'origin-when-cross-origin',
+  authors: [{ name: '사단법인 프리메드' }],
+  creator: '사단법인 프리메드',
+  publisher: '사단법인 프리메드',
+  metadataBase: new URL('https://tech.freemed.or.kr'),
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    title: '프리메드 기술 블로그',
+    description: '청년보건의료NGO 사단법인 프리메드 기술 블로그입니다.',
+    images: '/og-image.png',
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {

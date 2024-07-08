cfedb144ea0bfb3bb0a9a1a49a59e356226b0e74
kjjlss2 <jgim0417@gmail.com>
2024-07-08 22:24:15 +0900

    design: 태그 간격 크기 수정

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const Tag = ({ text }: { text: string }) => {
   return (
     <div className="mt-4 md:mt-0">
-      <div className="mb-2 mr-2 inline-block rounded bg-gray-50 p-2 dark:bg-gray-800">
+      <div className="mb-2 mr-2 inline-block rounded bg-gray-50 px-3 py-1 dark:bg-gray-800">
         <Link href={`/`}>
           <span className="body1">#{text}</span>
         </Link>
